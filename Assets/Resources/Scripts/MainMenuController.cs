4918f24ec8df6e76630fbe45213dc641148d6b80
bubblesses <nmpeckham@mun.ca>
2019-01-27 13:46:09 -0330

    etc

--- Assets/Resources/Scripts/MainMenuController.cs
+++ Assets/Resources/Scripts/MainMenuController.cs
@@ -7,15 +7,22 @@ using UnityEngine.SceneManagement;
 public class MainMenuController : MonoBehaviour
 {
     public Button startButton;
+    public Button creditsButton;
     // Start is called before the first frame update
     void Start()
     {
         startButton.onClick.AddListener(StartGame);
+        creditsButton.onClick.AddListener(LoadCredits);
     }
 
     void StartGame()
     {
         SceneManager.LoadScene("levelSelect");
+    }
+
+    void LoadCredits()
+    {
+        SceneManager.LoadScene("Credits");
     }
 
 }
